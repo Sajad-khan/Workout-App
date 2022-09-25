358409352967e0e3652f2634ad98494ad7bbb72d
sajadkhan <khansajad1910@outlook.com>
2022-09-25 14:21:59 +0530

    bug fixes

--- app/src/main/java/khan/sajad/example/letsworkout/ExerciseActivity.kt
+++ app/src/main/java/khan/sajad/example/letsworkout/ExerciseActivity.kt
@@ -3,20 +3,29 @@ package khan.sajad.example.letsworkout
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import khan.sajad.example.letsworkout.data.Exercises
 import khan.sajad.example.letsworkout.databinding.ActivityExerciseBinding
+import java.util.*
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private lateinit var binding: ActivityExerciseBinding
     private var position = 0
     private var exerciseList = Exercises().loadExercises()
+
+    // Text To speech variable
+    private lateinit var textToSpeech: TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        textToSpeech = TextToSpeech(this, this)
         setSupportActionBar(binding.exerciseToolbar)
         // enables back button on toolbar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -26,11 +35,16 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
             finish()
         }
+
         startTimer()
     }
 
     private fun startTimer(){
         binding.tvTitle.text = getString(R.string.get_ready, exerciseList[position].name)
+
+        // Speak out
+        textToSpeech.speak(binding.tvTitle.text, TextToSpeech.QUEUE_FLUSH, null, "")
+
         object : CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 binding.tvTimer.text = (millisUntilFinished / 1000).toString()
@@ -49,6 +63,8 @@ class ExerciseActivity : AppCompatActivity() {
             val exercise = exerciseList[position]
             binding.tvTitle30.text = exercise.name
             showGif(exercise.image)
+            textToSpeech.speak(binding.tvTitle30.text, TextToSpeech.QUEUE_FLUSH, null, "")
+
             object : CountDownTimer(30000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 binding.tvTimer30.text = (millisUntilFinished / 1000).toString()
@@ -91,4 +107,19 @@ class ExerciseActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+
+    // OnIt for text to speech
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val langSet = textToSpeech.setLanguage(Locale.ENGLISH)
+            if(langSet == TextToSpeech.LANG_MISSING_DATA || langSet == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS", "Unable set the language!")
+            }
+        }
+        else{
+            Log.e("TTS", "Failed to start Text to speech")
+        }
+    }
+
 }
